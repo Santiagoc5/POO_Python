25f16d5097ea8055486e67badcf34acdb635b7be
Santiagoc5 <claroscampos7@gmail.com>
2025-12-11 11:32:07 -0500

    Update main (1).py

--- Animales/main (1).py
+++ Animales/main (1).py
@@ -1,25 +1,22 @@
-from caballo import Caballo
-from cocodrillo import Cocodrilo
+from Caballo import Caballo
+from Cocodrilo import Cocodrilo
 from PezTropical import PezTropical
 from EscarabajoRinoceronte import EscarabajoRinoceronte
 from Pato import Pato
 
-def main():
-    caballo = Caballo("Relámpago", 5, "pradera", "pasto", "grande", "marrón", 60, "liso", True)
-    caballo.galopar()
+def principal():
+    
+    equino = Caballo("Trueno", 8, "pastizal", "heno", "medio", "blanco", 75, "rizado", False)
+    equino.ejecutar_carrera() 
 
-    croc = Cocodrilo("Fauces", 12, "río", "carnívoro", "grande", "verde", 2000, True, 4.5)
-    croc.sumergirse()
+    reptil = Cocodrilo("Mordisco", 25, "manglar", "pez y ave", "gigante", "gris oscuro", 3500, False, 5.8)
+    reptil.hundirse() 
 
-    pez = PezTropical("Coralito", 1, "arrecife", "plancton", "pequeño", "azul", 35, "redonda", True)
-    pez.nadar()
+    pececillo = PezTropical("Espectro", 2, "cueva marina", "algas", "minúsculo", "amarillo", 42, "triangular", False)
+    pececillo.desplazarse_liquido()
 
-    esc = EscarabajoRinoceronte("Titan", 2, "selva", "savia", "pequeño", "negro", 850, "duro", 3)
-    esc.excavar()
-
-    pato = Pato("Plumitas", 3, "lago", "omnivoro", "mediano", "verde",0.75, True, "hidrofóbico")
-    pato.nadar()
-
-
-if __name__ == "__main__":
-    main()
+    coleoptero = EscarabajoRinoceronte("Atlas", 1, "bosque seco", "fruta", "estándar", "marrón claro", 500, "semiblando", 1.5)
+    coleoptero.perforar_tierra() 
+    
+    ave_acuatica = Pato("Quakie", 6, "pantano", "vegetación", "grande", "marrón/negro", 1.2, False, "brillante")
+    ave_acuatica.flotar_en_agua()
