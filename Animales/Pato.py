d91fe972c12a60edb2ef2f2471a46a2ddc029efc
Santiagoc5 <claroscampos7@gmail.com>
2025-12-11 11:24:42 -0500

    Update Pato.py

--- Animales/Pato.py
+++ Animales/Pato.py
@@ -1,14 +1,14 @@
 from animal import Animal
 class Pato(Animal):
     def __init__(self, nombre, edad, habitat, dieta, tamaño, color,
-                 envergadura, migratorio, tipo_plumaje):
+                 amplitud_alar, requiere_migracion, clase_cubierta_plumas):
         super().__init__(nombre, edad, habitat, dieta, tamaño, color)
-        self.envergadura = envergadura
-        self.migratorio = migratorio
-        self.tipo_plumaje = tipo_plumaje
+        self.amplitud_alar = amplitud_alar
+        self.requiere_migracion = requiere_migracion
+        self.clase_cubierta_plumas = clase_cubierta_plumas
 
-    def nadar(self):
-        print(f"{self.nombre} se desliza sobre el agua.")
+    def flotar_en_agua(self):
+        print(f"{self.nombre} se desliza sobre la superficie liquida.")
 
-    def volar(self):
-        print(f"{self.nombre} emprende vuelo con elegancia.")
+    def iniciar_vuelo(self):
+        print(f"{self.nombre} alza el vuelo con gracia.")
