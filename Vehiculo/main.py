b839c0296eef30ee42df6b374ba8d7b423f02746
Santiagoc5 <claroscampos7@gmail.com>
2025-12-11 11:42:21 -0500

    Update main.py

--- Vehiculo/main.py
+++ Vehiculo/main.py
@@ -1,18 +1,15 @@
-from carro_deportivo import CarroDeportivo
-from camioneta_carga import CamionetaCarga
+from CarroDeportivo import CarroDeportivo
+from CamionetaCarga import CamionetaCarga
 
-def main():
-    deportivo = CarroDeportivo("BMW Z4", "Negro", "3.0 Turbo", 2, 2, "Gasolina")
-    camioneta = CamionetaCarga("Hyundai H100", "Blanca", "2.5 Diesel", 4, 3, "Diesel", 1500)
+def main(): 
+    
+    vehiculo_rapido = CarroDeportivo("Porsche 911", "Rojo Fuego", "4.0 Atmosférico", 2, 2, "Premium")
+    unidad_utilitaria = CamionetaCarga("Ford Transit", "Gris Plomo", "2.0 Turbo Diesel", 3, 2, "Diesel", 2200)
 
-    print(deportivo.descripcion())
-    print(deportivo.aceleracion_frenado())
-    print(deportivo.info_extra())
+    print(vehiculo_rapido.obtener_resumen())        
+    print(vehiculo_rapido.modificar_velocidad())         
+    print(vehiculo_rapido.detalles_adicionales())       
 
-    print()
-
-    print(camioneta.descripcion())
-    print(camioneta.climatizacion())
-    print(camioneta.info_extra())
-
-main()
+    print(unidad_utilitaria.obtener_resumen())          
+    print(unidad_utilitaria.regulacion_temperatura())    
+    print(unidad_utilitaria.detalles_adicionales())      
