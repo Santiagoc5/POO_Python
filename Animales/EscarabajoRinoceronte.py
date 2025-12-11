544329c1c9655f0cb4a9397aca245c1966842c4b
Santiagoc5 <claroscampos7@gmail.com>
2025-12-11 11:23:55 -0500

    Refactor EscarabajoRinoceronte class attributes and methods

--- Animales/EscarabajoRinoceronte.py
+++ Animales/EscarabajoRinoceronte.py
@@ -1,14 +1,14 @@
 from animal import Animal
 class EscarabajoRinoceronte(Animal):
     def __init__(self, nombre, edad, habitat, dieta, tamaño, color,
-                 fuerza_relativa, exoesqueleto, longitud_cuerno):
+                 potencia_comparativa, cubierta_rigida, extension_apendice_frontal):
         super().__init__(nombre, edad, habitat, dieta, tamaño, color)
-        self.fuerza_relativa = fuerza_relativa
-        self.exoesqueleto = exoesqueleto
-        self.longitud_cuerno = longitud_cuerno
+        self.potencia_comparativa = potencia_comparativa
+        self.cubierta_rigida = cubierta_rigida
+        self.extension_apendice_frontal = extension_apendice_frontal
 
-    def excavar(self):
-        print(f"{self.nombre} excava con determinación.")
+    def perforar_tierra(self):
+        print(f"{self.nombre} perfora la tierra con resolución.")
 
-    def levantar(self):
-        print(f"{self.nombre} demuestra su fuerza impresionante.")
+    def ejecutar_fuerza(self):
+        print(f"{self.nombre} demuestra su potencia formidable.")
